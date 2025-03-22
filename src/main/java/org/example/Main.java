4693d53ee0ef2a3358e75535f4db136eac696a92
kqjm1 <kqk9269@gmail.com>
2025-03-22 23:16:47 +0900

    매개변수없이 공격자면 바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ class Main {
     public static void main(String[] args) {
         전사 a전사 = new 전사();
 
-        String name = "칸";
-        a전사.name = name; // 클래스 안 변수들은 이름이 겹쳐도 괜찮음
+        String 이름 = "칸";
+        a전사.name = 이름;
         a전사.age = 20;
         a전사.자기소개();
 
@@ -16,16 +16,16 @@ class Main {
         a전사.자기소개();
 
         a전사.age = 30;
-        a전사.name = "카니";
+        a전사.name = "진";
         a전사.자기소개();
 
         a전사.a무기 = new 활();
         a전사.공격();
-        // 출력 : 카니가 활로 공격합니다.
+        //출력 : 진이 활로 공격합니다.
 
         a전사.a무기 = new 칼();
         a전사.공격();
-        // 출력 : 카니가 칼로 공격합니다.
+        //출력 : 진이 칼로 공격합니다.
     }
 }
 class 전사{
@@ -34,6 +34,7 @@ class 전사{
     무기 a무기; // class 전사가 가지고있는 무기타입의 이름이 a인 변수
 
     void 공격(){
+        a무기.공격자 = this.name;
         a무기.작동(); //변수 타입이 가지고있는 메서드 실행
     }
     void 자기소개(){
@@ -41,6 +42,7 @@ class 전사{
     }
 }
 class 무기{
+    String 공격자;
     void 작동(){
 
     }
@@ -48,11 +50,12 @@ class 무기{
 }
 class 활 extends 무기{
     void 작동(){
-        System.out.println("카니가 활로 공격합니다.");
+
+        System.out.println(공격자+" 활로 공격합니다.");
     }
 }
 class 칼 extends 무기{
     void 작동(){
-        System.out.println("카니가 칼로 공격합니다.");
+        System.out.println(공격자+" 칼로 공격합니다.");
     }
 }
