3b151013f8ef0f89a764094a036d66c30b4007e6
kqjm1 <kqk9269@gmail.com>
2025-03-23 20:24:26 +0900

    인력사무소

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,61 +1,66 @@
 package org.example;
 
-import java.util.Scanner;
+// 문제 : 아래가 실행되도록 해주세요.
+// 조건 : 배열을 사용할 수 없습니다.
 
+import java.util.ArrayList;
+import java.util.List;
 
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        사람인력관리소 a사람인력관리소 = new 사람인력관리소();
 
-        String 이름 = "칸";
-        a전사.name = 이름;
-        a전사.age = 20;
-        a전사.자기소개();
+        a사람인력관리소.add사람("홍길순", 33);
+        // 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
+        a사람인력관리소.add사람("홍길동", 20);
+        // 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
+        a사람인력관리소.add사람("임꺽정", 30);
+        // 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
 
-        a전사.age++;
-        a전사.자기소개();
+        사람 a사람1 = a사람인력관리소.get사람(1);
+        a사람1.자기소개();
+        // 저는 1번, 홍길순, 33살 입니다.
 
-        a전사.age = 30;
-        a전사.name = "진";
-        a전사.자기소개();
+        사람 a사람2 = a사람인력관리소.get사람(2);
+        a사람2.자기소개();
+        // 저는 2번, 홍길동, 20살 입니다.
 
-        a전사.a무기 = new 활();
-        a전사.공격();
-        //출력 : 진이 활로 공격합니다.
-
-        a전사.a무기 = new 칼();
-        a전사.공격();
-        //출력 : 진이 칼로 공격합니다.
+        사람 a사람3 = a사람인력관리소.get사람(3);
+        a사람3.자기소개();
+        // 저는 3번, 임꺽정, 30살 입니다.
     }
 }
-class 전사{
-    int age;
+class 사람인력관리소{
+    int 마지막_번호;
+    List<사람>명단 = new ArrayList<>();
+
+    사람 사람;
+
+
+    void add사람(String name, int age) {
+        int 번호=마지막_번호+1;
+        사람 a사람 = new 사람();
+
+        a사람.age=age;
+        a사람.name=name;
+        a사람.번호=번호;
+
+        명단.add(a사람);
+
+        System.out.printf("나이가 %d살인 %d번째 사람(%s)이 추가되었습니다.\n",age,번호,name);
+
+        마지막_번호++;
+    }
+    사람 get사람(int 번호){
+        return 명단.get(번호-1);
+    }
+}
+class 사람 {
     String name;
-    무기 a무기; // class 전사가 가지고있는 무기타입의 이름이 a인 변수
+    int age;
+    int 번호;
 
-    void 공격(){
-        a무기.공격자 = this.name;
-        a무기.작동(); //변수 타입이 가지고있는 메서드 실행
-    }
     void 자기소개(){
-        System.out.println("안녕하세요 저는 "+this.age+"살 "+this.name+"입니다.");
-    }
-}
-class 무기{
-    String 공격자;
-    void 작동(){
-
-    }
-
-}
-class 활 extends 무기{
-    void 작동(){
-
-        System.out.println(공격자+" 활로 공격합니다.");
-    }
-}
-class 칼 extends 무기{
-    void 작동(){
-        System.out.println(공격자+" 칼로 공격합니다.");
+        System.out.printf("저는 %d번, %d, %s입니다.\n",번호,age,name);
     }
 }
