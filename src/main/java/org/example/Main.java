d56fd15a9bb7a7905206b3fabe9ffb6db505c429
kqjm1 <kqk9269@gmail.com>
2025-03-24 22:27:13 +0900

    수정과 삭제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,8 +16,12 @@ class Main {
         while (true) {//exit가 아닐때 반복
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
+
+
             if (cmd.length() == 0) {continue;}
+
             if (cmd.equals("exit")) {break;}
+
             if (cmd.equals("article write")) {
                 System.out.println("게시글을 작성합니다");
                 int id = lastid + 1; // 게시글 작성 순서번호
@@ -33,7 +37,8 @@ class Main {
                 System.out.printf("%d번 게시글이 생성되었습니다.\n", id);
                 lastid = id;
             }
-            if (cmd.equals("article list")){
+
+            else if (cmd.equals("article list")){
                 System.out.println("=======================================");
                 if(save.size()==-0){
                     System.out.println("게시글 없음");
@@ -45,7 +50,8 @@ class Main {
                     }
                 }
             }
-            if (cmd.startsWith("article detail")){
+
+            else if (cmd.startsWith("article detail")){
                 System.out.println("====상세보기====");
                 int ii = Integer.parseInt(cmd.split(" ")[2]); // 공백 기준으로 자르면 2번 인덱스가 정수가 옴
                 Article findar = null;
@@ -56,12 +62,56 @@ class Main {
                 }
                 if (findar == null) {
                     System.out.println("게시글 없음");
+                    continue;
                 }
                 System.out.println("번호 : " + findar.getId());
                 System.out.println("제목 : " + findar.getTitle());
                 System.out.println("내용 : " + findar.getBody());
             }
+
+            else if (cmd.startsWith("article delete")){
+                System.out.println("====삭제===");
+                int ii = Integer.parseInt(cmd.split(" ")[2]);
+                Article findar = null;
+                for (Article article : save) {
+                    if (article.getId() == ii){
+                        findar = article;
+                        break;}
+                }
+                if (findar == null) {
+                    System.out.println("게시글 없음");
+                    continue;
+                }
+                save.remove(findar);
+                System.out.println(ii+"번 게시글을 삭제");
+            }
+            else if (cmd.startsWith("article modify")){
+                System.out.println("====수정===");
+                int ii = Integer.parseInt(cmd.split(" ")[2]);
+                Article findar = null;
+                for (Article article : save) {
+                    if (article.getId() == ii){
+                        findar = article;
+                        break;}
+                }
+                System.out.println("기존제목: "+findar.getTitle());
+                System.out.println("기존내용: "+findar.getBody());
+                System.out.print("new 제목: ");
+                String newtitle = sc.nextLine().trim();
+                System.out.print("new 내용: ");
+                String newbody = sc.nextLine().trim();
+
+                findar.setTitle(newtitle);
+                findar.setBody(newbody);
+
+                System.out.println(ii+"번 게시글 수정");
+            }else {
+                System.out.println("사용불가");
+
+            }
         }
+        System.out.println("==프로그램 종료==");
+        sc.close();
     }
 }
 class Article{
