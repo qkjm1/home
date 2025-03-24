666e6da3e6d367bf5800b73f3dae3ead46ac0e37
kqjm1 <kqk9269@gmail.com>
2025-03-24 21:12:17 +0900

    게시글 입력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,81 @@
 package org.example;
-import java.util.Arrays;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // 일반
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==실행==");
+        List<Article>save=new ArrayList<>(); // 조합을 저장
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] *= 2;
+        int lastid = 0;
+
+        while (true) {//exit가 아닐때 반복
+            System.out.print("명령어) ");
+            String cmd = sc.nextLine().trim();
+            if (cmd.length() == 0) {continue;}
+            if (cmd.equals("exit")) {break;}
+            if (cmd.equals("article write")) {
+                System.out.println("게시글을 작성합니다");
+                int id = lastid + 1; // 게시글 작성 순서번호
+                //입력
+                System.out.print("제목: ");
+                String title = sc.nextLine().trim();
+                System.out.print("내용: ");
+                String body = sc.nextLine().trim();
+                //저장     저장할 조합(생성자)가 필요
+                Article articles = new Article(id, title, body); // 생성자
+                save.add(articles); // 조합을 저장
+
+                System.out.printf("%d번 게시글이 생성되었습니다.\n", id);
+                lastid = id;
+            }
+            if (cmd.equals("article list")){
+                System.out.println("=======================================");
+                if()
+                for (Article article : save) { // save가 순회
+
+
+
+
+                }
+            }
         }
+    }
+}
+class Article{
+    int id;
+    String title;
+    String body;
+    Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+    public int getId() {
+        return id;
+    }
 
-        System.out.println(Arrays.toString(arr));
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        // 스트림
-        arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public String getTitle() {
+        return title;
+    }
 
-        int[] resultArr = Arrays.stream(arr)
-                .map(e -> {
-                    return e * 2;
-                })
-                .toArray();
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        resultArr = Arrays.stream(arr)
-                .map(e -> e * 2) // 이런식으로 줄여쓸 수 있다.
-                .toArray();
+    public String getBody() {
+        return body;
+    }
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(resultArr));
+    public void setBody(String body) {
+        this.body = body;
     }
 }
