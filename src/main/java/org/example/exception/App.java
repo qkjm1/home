a4a66071118a1347d9244d207080ad1a1392c81f
kqjm1 <kqk9269@gmail.com>
2025-04-08 20:52:02 +0900

    crud

---
+++ src/main/java/org/example/exception/App.java
@@ -0,0 +1,13 @@
+package org.example.exception;
+
+import java.util.Scanner;
+
+public class App {
+    private Scanner sc;
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+    public void run(){
+        System.out.println("프로그램실행");
+    }
+}
